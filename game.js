67b50cf8c84a3ed6d0b45e0131d703943b751b21
USERNAME <chacha.agostinelli@gmail.com>
2023-06-06 14:37:39 +0200

    6/06

--- game.js
+++ game.js
@@ -7,6 +7,7 @@ loadSprite("tube", "sprites/pipe-red.png")
 loadSprite("gameover", "sprites/gameover.png")
 loadSprite("message", "sprites/message.png")
 loadSprite("bg2", "sprites/background-day.png")
+loadSprite("coin", "sprites/undefined - Imgur.png")
 
 
 
@@ -15,10 +16,7 @@ setGravity(2400)
 loadBean()
 
 /*========================= Scene du message du debut !  ========================*/
-
-
 scene("message", ()=> {
-
 
     add([
         sprite("bg", {width: width(), height: height()}),
@@ -31,9 +29,6 @@ scene("message", ()=> {
         anchor("center"),
     ]);
 
-
-
-
     onKeyPress("space", () => go("game"))
     onClick(() => go("game"))
 
@@ -41,12 +36,13 @@ scene("message", ()=> {
 
 go("message")
 
-
-
-
 /*========================= Scene du jeu  ========================*/
 
 scene("game", () => {
+
+    const game = add([
+        timer(),
+    ])
 
     const TUBE_OPEN = 240
     const TUBE_MIN = 60
@@ -57,43 +53,36 @@ scene("game", () => {
 /*l'ajout du background*/
     add([
         sprite("bg", {width: width(), height: height()}),
-
+        "bg"
     ]);
-
     /*l'ajout de l'oiseau */
 
     const bird = add([
         sprite("bird", {height: 50}),
         pos(width() / 4, 0),
         area(),
+        z(2),
         body(),
     ])
-
     /*"Parametre de l'oiseau"*/
     bird.onUpdate(() => {
         if (bird.pos.y >= height() || bird.pos.y <= CEILING) {
             go("lose", score)
         }
     })
-
-    /*configuration des touche pour sauter*/
+    /*configuration des touches pour sauter*/
 
     onKeyPress("space", () => {
         bird.jump(JUMP_FORCE)
-
     })
 
     onGamepadButtonPress("south", () => {
         bird.jump(JUMP_FORCE)
-
     })
-
 
     onClick(() => {
         bird.jump(JUMP_FORCE)
-
     })
-
     /*Fonction qui fait apparaitre les tube aléatoirement */
 
     function spawntube() {
@@ -110,25 +99,33 @@ scene("game", () => {
             outline(4),
             area(),
             move(LEFT, SPEED),
+            z(2),
             offscreen({ destroy: true }),
-
             "tube",
         ])
 
         add([
             pos(width(), h1 + TUBE_OPEN),
             rect(64, h2),
-            sprite('tube'),
+            color(0, 127, 255),
             outline(4),
             area(),
+            z(2),
             move(LEFT, SPEED),
             offscreen({ destroy: true }),
-
             "tube",
-
             { passed: false },
         ])
 
+       add([
+            sprite("coin", {height: 50}),
+            pos(width(), h1 + 90 ),
+            area(),
+           z(2),
+            move(LEFT, SPEED),
+            offscreen({ destroy: true }),
+           "coin"
+        ]);
     }
 
 
@@ -137,14 +134,16 @@ scene("game", () => {
         addKaboom(bird.pos)
     })
 
+    bird.onCollide("coin", (c) => {
+       addScoreCoin()
+        destroy(c)
+    })
+
     onUpdate("tube", (p) => {
         if (p.pos.x + p.width <= bird.pos.x && p.passed === false) {
             addScore()
             p.passed = true
         }
-
-
-
     })
 
     loop(1, () => {
@@ -163,33 +162,116 @@ scene("game", () => {
     function addScore() {
         score++
         scoreLabel.text = score
-
     }
 
 
+
+
+
+    /*score des pieces =   */
+    let scoreCoin = 0
+
+    const scoreCoinLabel = add([
+
+        text(scoreCoin),
+        pos(width() / 20, 80),
+        fixed(),
+        z(100),
+    ])
+    const CoinLabel = add([
+        sprite("coin", {height: 50}),
+        pos(width() / 50, 78),
+        fixed(),
+        z(100),
+    ])
+
+    function addScoreCoin() {
+        scoreCoin = scoreCoin+2
+        scoreCoinLabel.text = scoreCoin
+
+
+    }
+
+    let curTween = null
+
+
+    onKeyPress("p", () => {
+        game.paused = !game.paused
+        if (curTween) curTween.cancel()
+        curTween = tween(
+
+            pauseMenu.pos,
+
+            game.paused ? center() : center().add(0, 700),
+
+            0.5,
+
+            (p) => pauseMenu.pos = p,
+
+            easings.easeOutElastic,
+        )
+        if (game.paused) {
+            pauseMenu.hidden = false
+            pauseMenu.paused = false
+        } else {
+            curTween.onEnd(() => {
+                pauseMenu.hidden = true
+                pauseMenu.paused = true
+            })
+        }
+    })
+
+    const pauseMenu = add([
+        rect(300, 400),
+        color(255, 255, 255),
+        outline(4),
+        anchor("center"),
+        z(200),
+        pos(center().add(0, 700)),
+    ])
+
+    pauseMenu.hidden = true
+    pauseMenu.paused = true
+
+    onUpdate("bg", (bg) => {
+        if (score > 1) {
+            destroy(bg)
+            add([
+                sprite("bg2", {width: width(), height: height()}),
+                z(0)
+            ]);
+        }
+    })
 })
-
-
 
 /*=========================Scene qui affiche le texte quand on perd ========================*/
 scene("lose", (score) => {
 
-
     add([
         sprite("bg", {width: width(), height: height()}),
-
     ]);
     add([
         sprite("gameover"),
         pos(width() / 2, height() / 2 - 300),
         scale(3),
         anchor("center"),
-
     ]);
     add([
         sprite("bird"),
         pos(width() / 2, height() / 2 - 108),
         scale(3),
+        anchor("center"),
+    ])
+    add([
+        sprite("coin"),
+        pos(width() / 1.90, height() / 2 - -5),
+        scale(3),
+        anchor("center"),
+    ])
+    add([
+        text(score*2),
+        pos(width() / 2, height() / 2 - 0),
+        scale(1.25),
         anchor("center"),
     ])
 
@@ -202,8 +284,11 @@ scene("lose", (score) => {
 
     onKeyPress("space", () => go("game"))
     onClick(() => go("game"))
-
 })
+
+
+
+
 
 go("game")
 go("message")
